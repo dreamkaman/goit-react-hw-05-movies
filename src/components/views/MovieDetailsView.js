45964c42dbed0f45ac79bde889cc3f03a301e2db
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-09 11:53:30 +0200

    next

--- src/components/views/MovieDetailsView.js
+++ src/components/views/MovieDetailsView.js
@@ -1,12 +1,54 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import moviesAPI from '../../services/api';
 
 function MovieDetailsView() {
-  const { movieId } = useParams;
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  console.log(movieId);
+
+  useEffect(() => {
+    moviesAPI.getDetails(movieId).then(responce => {
+      console.log(responce.data);
+      setMovie(responce.data);
+    });
+  }, []);
 
   return (
-    <div>
-      <h2>Here will be a Bookname!</h2>
-    </div>
+    <>
+      <Link to="/">Go Back</Link>
+
+      {movie && (
+        <div>
+          <div>
+            <img
+              src={`https://www.themoviedb.org/t/p/original${movie.poster_path}`}
+              alt={'poster'}
+              width={'300px'}
+            />
+            <div>
+              <h1>{movie.title}</h1>
+              <p>{`User Score: ${movie.vote_average}`}</p>
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+              <h3>Genres</h3>
+              <p>{movie.genres.map(genre => genre.name).join(', ')}</p>
+            </div>
+          </div>
+          <div>
+            <p>Additional information</p>
+            <ul>
+              <li>
+                <Link>Cast</Link>
+              </li>
+              <li>
+                <Link>Reviews</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/services/api/api.js
+++ src/services/api/api.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const API_KEY = 'c5d38a7be9bd3b66714f62495d2f9344';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
-// const baseParams = { api_key: API_KEY };
+const baseParams = { api_key: API_KEY };
 
 function getPopular() {
-  return axios.get('https://api.themoviedb.org/3/movie/popular', { params: { api_key: API_KEY } });
+  return axios.get('https://api.themoviedb.org/3/movie/popular', { params: baseParams });
 }
 
 function getDetails(id) {
-  return axios.get(BASE_URL & `movie/${id}`, { params: { api_key: API_KEY } });
+  return axios.get(`https://api.themoviedb.org/3/movie/${id}`, { params: baseParams });
 }
 
 const moviesAPI = {
