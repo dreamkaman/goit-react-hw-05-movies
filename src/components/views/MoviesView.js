ff2ad048f1e106dd4fdcf08867fd8e3da1fe9a1e
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-01-10 20:41:54 +0200

    next

--- src/components/views/MoviesView.js
+++ src/components/views/MoviesView.js
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import moviesAPI from '../../services/api';
+
+import List from '../List';
 const MoviesView = () => {
-  return <h2>This is movies page!</h2>;
+  const [state, setState] = useState({ query: '', movies: null });
+  const { query, movies } = state;
+  // useEffect(() => {
+  //   // if (query) { setQuery() }
+  // }, [query]);
+
+  const handleOnChange = e => {
+    setState(prevstate => ({ ...prevstate, query: e.target.value }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // console.log('Submit button pushed!');
+    moviesAPI.getMovieByName(query).then(responce => {
+      console.log(responce.data.results);
+      setState(prevstate => ({ ...prevstate, movies: responce.data.results }));
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={query} onChange={handleOnChange} />
+        <button type="Submit">Search</button>
+      </form>
+      <>
+        {!!movies && (
+          <List
+            elements={movies.map(movie => (
+              <li key={movie.id}>
+                <Link to={`/?query=${movie.title}`}>{movie.title}</Link>
+              </li>
+            ))}
+          />
+        )}
+      </>
+    </>
+  );
 };
 
 export default MoviesView;
--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,23 +1,27 @@
 import axios from 'axios';
 
 const API_KEY = 'c5d38a7be9bd3b66714f62495d2f9344';
-const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const params = { api_key: API_KEY };
 
 function getPopular() {
-  return axios.get(`${BASE_URL}popular`, { params });
+  return axios.get(`${BASE_URL}movie/popular`, { params });
 }
 
 function getDetails(movieId) {
-  return axios.get(`${BASE_URL}${movieId}`, { params });
+  return axios.get(`${BASE_URL}movie/${movieId}`, { params });
 }
 
 function getCast(movieId) {
-  return axios.get(`${BASE_URL}${movieId}/credits`, { params });
+  return axios.get(`${BASE_URL}movie/${movieId}/credits`, { params });
 }
 
 function getReviews(movieId) {
-  return axios.get(`${BASE_URL}${movieId}/reviews`, { params });
+  return axios.get(`${BASE_URL}movie/${movieId}/reviews`, { params });
+}
+
+function getMovieByName(query) {
+  return axios.get(`${BASE_URL}search/movie`, { params: { ...params, query } });
 }
 
 const moviesAPI = {
@@ -25,6 +29,7 @@ const moviesAPI = {
   getDetails,
   getCast,
   getReviews,
+  getMovieByName,
 };
 
 export default moviesAPI;
